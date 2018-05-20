dedc6b67496a07c7a82df98d2ad07772f2aff775
yostaka <ytacka@gmail.com>
2018-05-20 12:44:37 +0900

    make binary thresholding a function

--- CarND/lanedetection.py
+++ CarND/lanedetection.py
@@ -4,6 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import CarND.perstransform as cntransform
+import CarND.thresholding as cnthresh
+
+
+def getThresholdedBinaryImage(img, ksize=15, show_img=False):
+    gradx = cnthresh.abs_sobel_thresh(img, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+    grady = cnthresh.abs_sobel_thresh(img, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+    mag_binary = cnthresh.mag_thresh(img, sobel_kernel=3, mag_thresh=(30, 100))
+    dir_binary = cnthresh.dir_threshold(img, sobel_kernel=15, thresh=(0.7, 1.3))
+    color_binary = cnthresh.color_threshold(img, thresh=(170, 255))
+
+    combined = np.zeros_like(dir_binary, dtype='uint8')
+    combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | (color_binary == 1)] = 255
+
+    if show_img:
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+        f.tight_layout()
+        ax1.imshow(img)
+        ax1.set_title('Original Image', fontsize=50)
+        ax2.imshow(combined, cmap='gray')
+        ax2.set_title('Combined Thresholded Image', fontsize=50)
+        plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+        plt.show()
+
+    return combined
 
 
 def getLaneMaskImage(img, show_img=False):
--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
-import CarND.thresholding as cnthresh
 import CarND.calibration as cncalib
-import CarND.perstransform as cntransform
 import CarND.lanedetection as cnld
 
 
@@ -36,30 +34,10 @@ for idx, fname in enumerate(images):
 # Use color transforms, gradients, etc., to create a thresholded binary image
 # Read in an image
 images = glob.glob('test_images/*.jpg')
-ksize = 15 # Sobel kernel size - Choose a larger odd number to smooth gradient measurements
 
 for idx, fname in enumerate(images):
     img = mpimg.imread(fname)
-
-    # Apply each of the thresholding functions
-    gradx = cnthresh.abs_sobel_thresh(img, orient='x', sobel_kernel=ksize, thresh=(20, 100))
-    grady = cnthresh.abs_sobel_thresh(img, orient='y', sobel_kernel=ksize, thresh=(20, 100))
-    mag_binary = cnthresh.mag_thresh(img, sobel_kernel=3, mag_thresh=(30, 100))
-    dir_binary = cnthresh.dir_threshold(img, sobel_kernel=15, thresh=(0.7, 1.3))
-    color_binary = cnthresh.color_threshold(img, thresh=(170, 255))
-
-    combined = np.zeros_like(dir_binary, dtype='uint8')
-    combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | (color_binary == 1)] = 255
-
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    f.tight_layout()
-    ax1.imshow(img)
-    ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(combined, cmap='gray')
-    ax2.set_title('Combined Thresholded Image', fontsize=50)
-    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-
-    plt.show()
+    combined = cnld.getThresholdedBinaryImage(img, show_img=True)
 
     write_name = './undist_images/' + 'thresh_' + fname.split('/')[-1].split('.')[0] + '.jpg'
     cv2.imwrite(write_name, combined)
